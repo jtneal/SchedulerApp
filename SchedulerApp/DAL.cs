7a8a2113fa68e72fcf0184b4e1e378f3738bc1d0
Jason Neal <5340961+jtneal@users.noreply.github.com>
2025-04-15 00:31:42 -0400

    clean up with dal

---
+++ SchedulerApp/DAL.cs
@@ -0,0 +1,71 @@
+﻿using MySql.Data.MySqlClient;
+using SchedulerApp.Models;
+using System.Data;
+using System.Diagnostics;
+
+namespace SchedulerApp
+{
+    public class DAL
+    {
+        private readonly string _connectionString;
+
+        public DAL()
+        {
+            _connectionString = "server=localhost;user=root;database=client_schedule;port=3306;password=root";
+        }
+
+        public User GetUser(string username, string password)
+        {
+            var nullUser = new User()
+            {
+                userId = int.MinValue,
+                userName = string.Empty,
+                password = string.Empty,
+                active = int.MinValue,
+                createDate = DateTime.MinValue,
+                createdBy = string.Empty,
+                lastUpdate = DateTime.MinValue,
+                lastUpdateBy = string.Empty,
+            };
+
+            using var connection = new MySqlConnection(_connectionString);
+
+            try
+            {
+                connection.Open();
+                var query = "SELECT * FROM user WHERE active = 1 AND userName = @username AND password = @password LIMIT 1";
+                using var command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("username", username);
+                command.Parameters.AddWithValue("password", password);
+                using var reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    return new User()
+                    {
+                        userId = reader.GetInt32("userId"),
+                        userName = reader.GetString("userName"),
+                        password = reader.GetString("password"),
+                        active = reader.GetInt32("active"),
+                        createDate = reader.GetDateTime("createDate"),
+                        createdBy = reader.GetString("createdBy"),
+                        lastUpdate = reader.GetDateTime("lastUpdate"),
+                        lastUpdateBy = reader.GetString("lastUpdateBy"),
+                    };
+                }
+                else
+                {
+                    Debug.WriteLine("User not found");
+
+                    return nullUser;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+
+                return nullUser;
+            }
+        }
+    }
+}
--- SchedulerApp/LoginForm.cs
+++ SchedulerApp/LoginForm.cs
@@ -5,9 +5,12 @@ namespace SchedulerApp
 {
     public partial class LoginForm : Form
     {
+        private readonly DAL dal;
+
         public LoginForm()
         {
             InitializeComponent();
+            dal = new DAL();
         }
 
         private void textBox_TextChanged(object sender, EventArgs e)
@@ -17,34 +20,13 @@ namespace SchedulerApp
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-            // Move this to a repo or some other pattern
-            var connStr = "server=localhost;user=root;database=client_schedule;port=3306;password=root";
-            var conn = new MySqlConnection(connStr);
+            var user = dal.GetUser(usernameTextBox.Text, passwordTextBox.Text);
+            var isValid = user.userId > 0;
+            errorLabel.Visible = !isValid;
 
-            try
+            if (isValid)
             {
-                conn.Open();
-                var sql = "SELECT * FROM user WHERE active = 1 AND userName = @username AND password = @password LIMIT 1";
-                var cmd = new MySqlCommand(sql, conn);
-                cmd.Parameters.AddWithValue("username", usernameTextBox.Text);
-                cmd.Parameters.AddWithValue("password", passwordTextBox.Text);
-                var rdr = cmd.ExecuteReader();
-                var isValid = rdr.Read();
-                errorLabel.Visible = !isValid;
-                rdr.Close();
-
-                if (isValid)
-                {
-                    Debug.WriteLine("YOU IN BRO!");
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.ToString());
-            }
-            finally
-            {
-                conn.Close();
+                Debug.WriteLine("YOU IN BRO!");
             }
         }
     }
